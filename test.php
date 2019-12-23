02c033b5839a4b8009d4a22ad202867d03e0d2dd
Sergey Bakanov <sergeybknv@gmail.com>
2019-12-23 18:15:22 +0300

    remove test.php

--- test.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use GuzzleHttp\Client;
-
-require __DIR__.'/vendor/autoload.php';
-$guzzleClient = new Client();
-$res = $guzzleClient->request('GET', 'https://ya.ru');
-var_dump($res->getStatusCode());
